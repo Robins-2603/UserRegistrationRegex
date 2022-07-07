3cc626284090a3691931e7f3dc8fa86ff769fc8c
Robins-2603 <robinsforwork@gmail.com>
2022-07-08 03:21:22 +0530

    Added: first name validation prog

--- src/main/java/com/UserRegRegex/Operations.java
+++ src/main/java/com/UserRegRegex/Operations.java
@@ -1,4 +1,23 @@
 package com.UserRegRegex;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Operations {
+
+    public static void validFirstName() {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(name);
+        boolean b = m.matches();
+        if(b) {
+            System.out.println("You've Entered valid first name");
+        }
+        else {
+            System.out.println("You've Entered invalid first name");
+        }
+    }
 }
--- src/main/java/com/UserRegRegex/UserRegistrationMain.java
+++ src/main/java/com/UserRegRegex/UserRegistrationMain.java
@@ -1,7 +1,16 @@
 package com.UserRegRegex;
 
+import java.util.Scanner;
+
 public class UserRegistrationMain {
     public static void main(String[] args) {
         System.out.println("-----welcome to User Registration validation System------");
+        System.out.println("Enter 1: to validate first name");
+        Scanner scanner = new Scanner(System.in);
+        switch (scanner.nextInt()) {
+            case 1:
+                Operations.validFirstName();
+                break;
+        }
     }
 }
