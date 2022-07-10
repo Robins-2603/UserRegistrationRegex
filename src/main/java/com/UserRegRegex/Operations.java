55611a9cbf2e3ecaa1df30c48f49598502fd9a54
Robins-2603 <robinsforwork@gmail.com>
2022-07-10 23:44:06 +0530

    modified: password for atleast one special char

--- src/main/java/com/UserRegRegex/Operations.java
+++ src/main/java/com/UserRegRegex/Operations.java
@@ -81,7 +81,7 @@ public class Operations {
         Scanner scan =new Scanner(System.in);
         System.out.print("Enter your password: ");
         String password= scan.nextLine();
-        String regex="^[A-Z]{1,}[0-9]{1,}[A-Za-z0-9.@#$%^&*-_+.!]{6,}$";
+        String regex="^[A-Z]{1,}[.@#$%^&*_+!-]{1}[0-9]{1,}[A-Za-z0-9]{5,}$";
         Pattern p=Pattern.compile(regex);
         Matcher m=p.matcher(password);
         boolean result=m.matches();
