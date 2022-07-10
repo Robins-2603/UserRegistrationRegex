8e490ed20537ab340c33541e8a0194fb75a3f3ec
Robins-2603 <robinsforwork@gmail.com>
2022-07-10 23:39:00 +0530

    modified: password for atleast numeric num

--- src/main/java/com/UserRegRegex/Operations.java
+++ src/main/java/com/UserRegRegex/Operations.java
@@ -81,7 +81,7 @@ public class Operations {
         Scanner scan =new Scanner(System.in);
         System.out.print("Enter your password: ");
         String password= scan.nextLine();
-        String regex="^[A-Z]{1,}[A-Za-z0-9.@#$%^&*-_+.!]{7,}$";
+        String regex="^[A-Z]{1,}[0-9]{1,}[A-Za-z0-9.@#$%^&*-_+.!]{6,}$";
         Pattern p=Pattern.compile(regex);
         Matcher m=p.matcher(password);
         boolean result=m.matches();
